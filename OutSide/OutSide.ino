61ddaa441eb533042e819100a269f360fe4496cf
ja74687 <piotr.krakos@gmail.com>
2017-11-01 18:33:16 +0100

    Project paused

--- OutSide/OutSide.ino
+++ OutSide/OutSide.ino
@@ -1,39 +1,47 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <ArduinoJson.h>
-#include "DHT.h"
-#include <LowPower.h>
+//#include <LowPower.h>
 #include <VirtualWire.h>
 
-
+#define DHTPIN 2        
+#define DHTTYPE DHT11 
 #define transmiter_pin 6
-#define DHTPIN 2
-#define DHTTYPE DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
-int analogInput = A1;
+DHT_Unified dht(DHTPIN, DHTTYPE);
+StaticJsonBuffer<100> jsonBuffer;
+
+int analogInput = 1;
 float vout = 0.0;
 float vin = 0.0;
 float R1 = 30000.0;
 float R2 = 7500.0;
 int value = 0;
 int pinPower = 4;
-StaticJsonBuffer<100> jsonBuffer;
+
 void setup() {
- // Serial.begin(9600);
+  // Serial.begin(9600);
   dht.begin();
   vw_set_tx_pin(transmiter_pin);
   vw_setup(2000);
   pinMode(analogInput, INPUT);
-  delay(2000);
   pinMode(pinPower, OUTPUT);
   digitalWrite(pinPower, HIGH);
 }
 
 void loop() {
+  int temp = 0;
+  int humi = 0;
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  temp = event.temperature;
+  dht.humidity().getEvent(&event);
+  humi = event.relative_humidity;
+  sendInfo(temp, humi);
+  delay(30000);
+  //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 
-  sendInfo(dht.readTemperature(), dht.readHumidity());
-  delay(2000);
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  
 }
 
 void sendInfo(int temp, int humidity)
@@ -52,10 +60,13 @@ void sendInfo(int temp, int humidity)
 
 }
 
+
 double getBattery() {
   value = analogRead(analogInput);
   vout = (value * 5.0) / 1024.0;
   vin = vout / (R2 / (R1 + R2));
+  // Serial.println(vin);
   return (double) vin;
+
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Weather_Station
 
 
-## OutSide Controler Schematic
-![SchematicOutSide](Schematic/OutSide_Schematic.png)
+[b]Project has been stopped, bocuse DHT11 sensor had a very small precision. Project will be start when a new sensor HTU21D will be delivered to me.[/b]
